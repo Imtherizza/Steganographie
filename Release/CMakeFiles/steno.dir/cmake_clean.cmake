4f01a54b57bf41ea8fa6785a65bc45ddae3bee82
Eric Louangpraseuth <eric@MacBook-Air-de-Eric.local>
2023-01-13 09:58:02 +0100

    jtmbb

---
+++ Release/CMakeFiles/steno.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/steno.dir/main.c.o"
+  "CMakeFiles/steno.dir/main.c.o.d"
+  "steno"
+  "steno.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/steno.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Release/CMakeFiles/steno.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for steno.
+# This may be replaced when dependencies are built.
