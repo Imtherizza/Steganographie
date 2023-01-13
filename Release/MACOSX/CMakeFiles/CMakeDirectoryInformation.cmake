bcb70d1299e330af8e5ce623ac4271d5b2958b34
Eric Louangpraseuth <eric@MacBook-Air-de-Eric.local>
2023-01-13 11:51:13 +0100

    separate

--- Release/MACOSX/CMakeFiles/CMakeDirectoryInformation.cmake
+++ Release/MACOSX/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/Users/eric/Documents/steno/Steganographie")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/eric/Documents/steno/Steganographie/Release")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/eric/Documents/steno/Steganographie/Release/MACOSX")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
--- Release/MACOSX/cmake_install.cmake
+++ Release/MACOSX/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/eric/Documents/steno/Steganographie/Release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/eric/Documents/steno/Steganographie/Release/MACOSX/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
