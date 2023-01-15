38466f72241b5973dea78f9dc9e7d7240332dcbe
NTK <jean-cedric.petit@u-psud.fr>
2023-01-15 01:16:59 +0100

    Debut Header bitstream

---
+++ bitstream.h
@@ -0,0 +1,12 @@
+#ifndef __BITSTREAM_H__
+#define __BITSTREAM_H__
+
+typedef struct bitstream
+{
+    unsigned int size;
+    unsigned int position;
+    unsigned short *data;
+    
+}bitstream;
+
+#endif
