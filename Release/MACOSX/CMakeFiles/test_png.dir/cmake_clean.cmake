e56cd1dd4953e3a5cd9b0f32a3025be03612dd39
Eric Louangpraseuth <eric@MacBook-Air-de-Eric.local>
2023-01-27 08:39:56 +0100

    updated bitstream.h

---
+++ Release/MACOSX/CMakeFiles/test_png.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_png.dir/pngtest.c.o"
+  "CMakeFiles/test_png.dir/pngtest.c.o.d"
+  "CMakeFiles/test_png.dir/pngwrap.c.o"
+  "CMakeFiles/test_png.dir/pngwrap.c.o.d"
+  "test_png"
+  "test_png.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_png.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lib/bitstream.c
+++ lib/bitstream.c
@@ -3,10 +3,8 @@
 #include <png.h>
 #include "lib/bitstream.h"
 
-/*
- *
- *
- *
+/*  Creates an empty bitstream with no data, ready to use
+ *  @return The empty bistream
  */
 bitstream* CreateEmptyBitstream()
 {
@@ -15,49 +13,63 @@ bitstream* CreateEmptyBitstream()
     ret_bitstream->capacity = 0;
     ret_bitstream->position = 0;
     ret_bitstream->data = 0;
+    return ret_bitstream;
 }
 
-/*
- *
- *
- *
+/*  Creates a bitstream with data
+ *  @param array of data to put iunside the bitstream
+ *  @param size of data array
+ *  @return the bitstream with data inside
  */
 bitstream* CreateBitstreamOnData(char *data, unsigned int size)
 {
-    bitstream *ret_bitstream = malloc(sizeof(bitstream)*size);
+    bitstream *ret_bitstream = malloc(sizeof(bitstream));
     ret_bitstream->size = size;
     ret_bitstream->capacity = size/8;
     ret_bitstream->position = 0;
+    ret_bitstream->data = malloc(sizeof(short)*size)
     for(int i=0;i<data;i++)
         ret_bitstream->data[i] = data[i];
+    
+    return ret_bitstream;
 }
 
-/*
- *
+/*  could use b->data[b.position]
+ *  @param the bitstream to reset the position of
  */
 void ResetPosition(bitstream *b)
 {
     b->position = 0;
 }
 
-/*
+/* Assuming correct order
  *
  *
  *
  */
 void WriteBits(bitstream *b, char o, unsigned int nbbits)
 {
-
+    for(int i=b->position;i<nbbits;i++)
+    {
+        b->data[i]=o;
+    }
 }
 
 char ReadBits(bitstream *b, unsigned int nbbits)
 {
-
+    //????
 }
 
+/*  Reset the bitstream 
+ *
+ *
+ *
+ * 
+ */
 void FreeBitstream(bitstream *b)
 {
     if (b->data)
         free(b->data);
+    free(b);
 }
 
